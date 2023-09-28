9a50205fbd3de9cf81a5f79207f3251d410b81ad
Jean-Philippe Maquestiaux <maquejp@gmail.com>
2023-09-28 11:23:45 +0000

    Revised building base url

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://www.maquestiaux.net/dev/lebistro.me/',
   plugins: [react()],
 })
