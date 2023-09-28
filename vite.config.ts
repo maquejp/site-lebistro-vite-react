d9c44fef2639458b50a007b9cd2e241c30e9a480
Jean-Philippe Maquestiaux <maquejp@gmail.com>
2023-09-28 11:11:44 +0000

    Action + Configs

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: 'https://www.maquestiaux.net/dev/lebistro.me/',
   plugins: [react()],
 })
